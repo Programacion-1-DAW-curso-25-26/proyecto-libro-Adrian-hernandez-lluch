cb158eb5d5fb0d0ea7d629700e96af3aeae82789
Nombre <tu-email@dominio.com>
2026-01-20 09:13:20 +0100

    Implementado equals+ toString()

--- src/Libro.java
+++ src/Libro.java
@@ -1,19 +1,51 @@
+import java.util.Objects;
+
 public class Libro {
    private String titulo;
     private String autor;
     private int numPaginas;
     private int valoracion;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Libro libro = (Libro) o;
+        return isbn == libro.isbn;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(isbn);
+    }
+
+    //Con toString()imprimimos td
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", numPaginas=" + numPaginas +
+                ", valoracion=" + valoracion +
+                ", isbn=" + isbn +
+                '}';
+    }
+
+    private int isbn;
+
     public void mostrarInfo(){
         System.out.println("El "+titulo+" tiene "+numPaginas+" páginas.");
         System.out.println("El autor de este libro es +" +autor);
         System.out.println("La valoración de"+titulo+ " es de "+valoracion);
+        System.out.println("ISBN= "+isbn);
     }
-    public Libro(String titulo, String autor, int numPaginas, int valoracion ){
+
+
+
+    public Libro(String titulo, String autor, int numPaginas, int valoracion, Integer isbn ){
         this.titulo=titulo;
         this.autor=autor;
         this.numPaginas=numPaginas;
         this.valoracion=valoracion;
+        this.isbn=isbn;
     }
     public void setTitulo(){
         this.titulo= titulo;
@@ -38,5 +70,11 @@ public class Libro {
     }
     public int getValoracion(){
         return valoracion;
+    }
+    public int getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
     }
 }
--- src/MainLibro.java
+++ src/MainLibro.java
@@ -1,11 +1,17 @@
 public class MainLibro {
     public static void main(String[] args) {
         //Atributos del libro1
-        Libro libro1 = new Libro("La vida es dura","Iván Sanz",500,2);
+        Libro libro1 = new Libro("La vida es dura","Iván Sanz",500,2,2);
+        Libro libro3 = new Libro("La vida es dura","Iván Sanz",500,2,4);
         //Atributos del libro2
-        Libro libro2 = new Libro("The jungle","Massamagrell",1000,10);
+        Libro libro2 = new Libro("The jungle","Massamagrell",1000,10,6);
 
-
-
+        if (libro2.equals((libro1))){
+            System.out.println("Són iguales.");
+        }else {
+            System.out.println("Son diferentes.");
+        }
+        System.out.println(libro1);
+        System.out.println(libro2);
     }
 }
